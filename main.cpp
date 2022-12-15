cad9ba1df66d86691df838ac3a4a0c3dd687c1b6
Abhishek Kumar Gupta <satyam0350@gmail.com>
2022-12-15 17:37:54 +0530

    basic libs added

---
+++ main.cpp
@@ -0,0 +1,47 @@
+
+/*
+ *  Author : Abhishek Kumar Gupta
+ *
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define PI 3.1415926535897932384626433832795
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define pb push_back
+#define mk make_pair
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+#define sz(x) (int((x).size()))
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+//     cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+}
